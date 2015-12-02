066a5fd1cd1f791906a5f4f185f6a20a3e014e82
Julien Marcou <julien.marcou.dev@gmail.com>
2015-12-02 14:30:13 +0100

    Connect GUI to DatabaseManager

--- src/gui/App.java
+++ src/gui/App.java
@@ -5,25 +5,43 @@
  */
 package gui;
 
+import DatabasesManager.DatabaseManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 /**
  *
  * @author jmarcou
  */
 public class App extends Application {
-    
+
+    final DatabaseManager dbM;
+
+    public App() throws IOException {
+        dbM = new DatabaseManager();
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
-        
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
+        fxmlLoader.setController(new MainWindowController(dbM));
+        Parent root = fxmlLoader.load();
+
         Scene scene = new Scene(root);
-        
+
         stage.setScene(scene);
+        stage.setOnCloseRequest(event -> {
+            try {
+                dbM.clear();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
         stage.show();
     }
 
@@ -33,5 +51,5 @@ public class App extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
--- src/gui/MainWindowController.java
+++ src/gui/MainWindowController.java
@@ -5,31 +5,45 @@
  */
 package gui;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
+import DatabasesManager.DatabaseManager;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  *
  * @author jmarcou
  */
 public class MainWindowController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private Button executeQuery;
+
+    private final DatabaseManager dbM;
+
+    public MainWindowController(DatabaseManager dbM) {
+        this.dbM = dbM;
     }
-    
+
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+    public void initialize(URL location, ResourceBundle resources) {
+        executeQuery.setOnAction(event -> {
+            try {
+                dbM.print2DimTableInConsole(dbM.sendSQLDatabaseRequest("select * from Personne where Prenom = \"Aspen\";"));
+                dbM.print2DimTableInConsole(dbM.sendMongoRequest("db.EcoleMongoDB.find({IdEcole:\"5\"})"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("test");
+    }
 }
